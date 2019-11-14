1c0807d57e410a93ef92e3491917d3287cf38a57
MariamTalakhadze <49283747+MariamTalakhadze@users.noreply.github.com>
2019-11-14 20:40:10 +0400

    Service done

--- ASpBankUserApp/Bussiness/Service/OfferService.cs
+++ ASpBankUserApp/Bussiness/Service/OfferService.cs
@@ -9,7 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BusinessServices.Services
+namespace Bussiness.Service
 {
     public class OfferService : IOfferService
     {
--- ASpBankUserApp/Controllers/UserController.cs
+++ ASpBankUserApp/Controllers/UserController.cs
@@ -1,23 +1,46 @@
-﻿using System;
+﻿using Business.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Bussiness;
+using Bussiness.Service;
+using System.Web.Security;
+using ASpBankUserApp.Bussiness.IServiceInterface;
+using System.Net;
+
 
 namespace ASpBankUserApp.Controllers
 {
     public class UserController : Controller
     {
-        // GET: User
+
+        private UserService userdb = new UserService();
+        private GenderService genderdb = new GenderService();
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoginPost( String Name, String Password)
+        {
+            if(Name.Equals("admin") && Password.Equals("1"))
+            {
+                return View("index");
+            }
+            return View("Login");
         }
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult Registration()
+        {
+            return View();
+        }
+        public ActionResult RegistrationPost()
         {
             return View();
         }
